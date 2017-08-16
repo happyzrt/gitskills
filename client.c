50fe884cf9854cb18ac1da3fa780dd4253a18cf8
seven <xieshf@inspur.com>
2017-08-16 14:23:53 +0800

    update gitskills

--- client.c
+++ client.c
@@ -4,25 +4,41 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<unistd.h>
-void main(int argc,char** s){
-int sock_me,n;
-FILE *fp;
-struct sockaddr_in addr_serv;
-char buff[80];
-sock_me=socket(AF_INET,SOCK_STREAM,0);
-memset(&addr_serv,0,sizeof(addr_serv));
-addr_serv.sin_family=AF_INET;
-addr_serv.sin_port=htons(8000);
-inet_pton(AF_INET,s[1],&addr_serv.sin_addr);
-connect(sock_me,(struct sockaddr *)&addr_serv,sizeof(addr_serv));
-recv(sock_me,buff,80,0);
-printf("%s",buff);
-fp=fopen("out","wb");
-while((n=recv(sock_me,buff,80,0))!=0){
-fwrite(buff,1,n,fp);
-if(n<80)break;
-}
-printf("success\n");
-close(sock_me);
-fclose(fp);
+void main(int argc,char** s)
+{
+    int sock_me,n;
+    FILE *fp;
+    struct sockaddr_in addr_serv;
+    char buff[80];
+    sock_me=socket(AF_INET,SOCK_STREAM,0);
+    memset(&addr_serv,0,sizeof(addr_serv));
+    addr_serv.sin_family=AF_INET;
+    addr_serv.sin_port=htons(8000);
+    inet_pton(AF_INET,s[1],&addr_serv.sin_addr);
+    connect(sock_me,(struct sockaddr *)&addr_serv,sizeof(addr_serv));
+    {   //tcp连接服务器成功后，获取内核赋予socket的本地ip
+        char local_ip[20];
+        socklen_t serv_len = sizeof(addr_serv);
+        getsockname(sock_me,(struct sockaddr*)&addr_serv,&serv_len);
+        inet_ntop(AF_INET,&addr_serv.sin_addr,local_ip,sizeof(local_ip));
+        printf("local_ip:%s\tlocal_port:%d\n",local_ip,ntohs(addr_serv.sin_port));
+
+        getpeername(sock_me,(struct sockaddr*)&addr_serv,&serv_len);
+        inet_ntop(AF_INET,&addr_serv.sin_addr,local_ip,sizeof(local_ip));
+        printf("peer_ip:%s\tpeer_port:%d\n",local_ip,ntohs(addr_serv.sin_port));
+    }
+    recv(sock_me,buff,80,0);
+    printf("%s",buff);
+    fp=fopen("out","wb");
+    while((n=recv(sock_me,buff,80,0))!=0)
+    {
+        fwrite(buff,1,n,fp);
+        //printf("%d\n",n);
+        if(n<80)break;
+    }
+    send(sock_me,buff,80,0);
+    if(n==0)printf("7\n");
+    printf("success\n");
+    close(sock_me);
+    fclose(fp);
 }
--- server.c
+++ server.c
@@ -3,31 +3,83 @@
 #include<string.h>
 #include<unistd.h>
 #include<sys/socket.h>
+#include<sys/types.h>
 #include<netinet/in.h>
-void main(int argc,char ** s){
-int socket_server,socket_connect;
-struct sockaddr_in addr_serv;
-char buff[80];
-int n,sum=0;
-FILE *fp;
-socket_server=socket(AF_INET,SOCK_STREAM,0);
-memset(&addr_serv,0,sizeof(struct sockaddr_in));
-addr_serv.sin_family=AF_INET;
-addr_serv.sin_addr.s_addr=htonl(INADDR_ANY);
-addr_serv.sin_port=htons(8000);
-bind(socket_server,(struct sockaddr *)&addr_serv,sizeof(addr_serv));
-listen(socket_server,10);
-while(1){
-socket_connect=accept(socket_server,(struct sockaddr *)NULL,NULL);
-if(fork()==0){
-send(socket_connect,"hello,you are connected.......\ndownload.......\n",80,0);
-fp=fopen(s[1],"rb");
-while((n=fread(buff,1,80,fp))!=0)send(socket_connect,buff,n,0);
-fclose(fp);
-close(socket_connect);
-sum++;
-system("clear");
-printf("%d download\n",sum);
-}
-}
+#include<errno.h>
+int main(int argc,char ** s)
+{
+    int socket_server,socket_connect;
+    struct sockaddr_in addr_serv;
+    char buff[80];
+    int n,sum=0;
+    FILE *fp;
+    extern int errno;
+
+    //create server socket
+    socket_server=socket(AF_INET,SOCK_STREAM,0);
+    if(socket_server < 0)
+    {
+        printf("create socket error:%s\n",strerror(errno));
+        return -1;
+    }
+    memset(&addr_serv,0,sizeof(struct sockaddr_in));
+
+    //set the server address
+    addr_serv.sin_family=AF_INET;
+    addr_serv.sin_addr.s_addr=htonl(INADDR_ANY);
+    addr_serv.sin_port=htons(8000);
+
+    //server address bind to the server socket 
+    bind(socket_server,(struct sockaddr *)&addr_serv,sizeof(addr_serv));
+
+    //begin listening
+    listen(socket_server,10);
+    while(1)
+    {
+        sum=sum+1;
+        n = sizeof(addr_serv);
+        socket_connect=accept(socket_server,(struct sockaddr *)&addr_serv,&n);
+        if(socket_connect <= 0)
+        {
+            return -1;
+        }
+        if(fork()==0)
+        {
+            int ret_size;
+	        ret_size = send(socket_connect,"hello,you are connected.......\ndownloading.......\n",80,MSG_NOSIGNAL);
+            if(ret_size < 0)
+            {
+                if(errno == EAGAIN || errno == EWOULDBLOCK)
+                {
+                    return 0;
+                }
+            }
+	        fp = fopen(s[1],"rb");
+            if(NULL == fp)
+            {
+                return -1;
+            }
+	        while((n=fread(buff,1,80,fp))!=0)
+            {
+	            ret_size = send(socket_connect,buff,n,MSG_NOSIGNAL);
+                if(ret_size < 0)
+                {
+                    if(errno == EAGAIN || errno == EWOULDBLOCK)
+                    {
+                        return 0;
+                    }
+                    if(errno == EPIPE)
+                    {
+                        printf("send error:%s\n",strerror(errno));
+                    }
+                }
+	        }
+	        recv(socket_connect,buff,80,0);
+	        fclose(fp);
+	        close(socket_connect);
+	        printf("%d downloaded\n",sum);
+	        return 0;
+        }
+    }
+    return 0;
 }
